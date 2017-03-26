e9bf22a221adad390e319417f76e22898dd52da1
liguobao <codelover@qq.com>
2017-03-26 20:07:21 +0800

    add code

--- DungClientForDotNet/BLL/BizProxyIPInfo.cs
+++ DungClientForDotNet/BLL/BizProxyIPInfo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DungClientForDotNet.Hepler;
+using DungClientForDotNet.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,128 @@ namespace DungClientForDotNet.BLL
 {
     public class BizProxyIPInfo
     {
-        public static void LoadProxyIPInfo()
-        {
+        public string CheckURL { get; set; }
 
+        public string CheckDomain { get; private set; }
+
+        public int APIQueryPageCount { get; set; }
+
+
+        public void LoadProxyIPInfo()
+        {
+            var avAPIData = WebAPIHelper.GetAPIResult<AVAPIResult>(ConfigInfo.AVAPIURL
+                + $"?usedSign=&checkUrl={CheckURL}&domain={CheckDomain}&num={APIQueryPageCount}");
+            if(avAPIData!=null && avAPIData.data!=null && avAPIData.data.data != null)
+            {
+                Console.WriteLine(""+avAPIData.data.data.Count);
+            }
         }
+
+
+
+
+
+        #region public prop
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string IP { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProxyIP { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Port { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int IPValue { get; set; }
+        /// <summary>
+        /// 阿尔巴尼亚
+        /// </summary>
+        public string Country { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Area { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Region { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ISP { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CountryID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string AreaID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string RegionID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string CityID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ISPID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Transperent { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Speed { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ConnectionScore { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int AvailbelScore { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ConnectionScoreDate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int AvailbelScoreDate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Createtime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Lostheader { get; set; }
+        #endregion
+
+
+
     }
 }
--- DungClientForDotNet/Hepler/WebAPIHelper.cs
+++ DungClientForDotNet/Hepler/WebAPIHelper.cs
@@ -7,6 +7,16 @@ namespace DungClientForDotNet.Hepler
 {
     public class WebAPIHelper
     {
-
+        /// <summary>
+        /// 通过接口获取数据
+        /// </summary>
+        /// <typeparam name="T">返回数据类型</typeparam>
+        /// <param name="apiURL">接口AIP地址</param>
+        /// <returns>返回数据类型</returns>
+        public static T GetAPIResult<T>(string apiURL)
+        {
+            var resultHTML = HTTPHepler.GetByURL(apiURL);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(apiURL);
+        }
     }
 }
